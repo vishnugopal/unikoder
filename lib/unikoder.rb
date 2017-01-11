36360a6e61330c4fbaea7db6a82915eae6a48413
Vishnu Gopal <vishnu@sv.co>
2017-01-11 16:53:49 +0530

    Finishing up encode_string

--- lib/unikoder.rb
+++ lib/unikoder.rb
@@ -1,15 +1,22 @@
 class Unikoder
   STRING_STEGANOGRAPHY = [
     "\u2000", "\u2001", "\u2002", "\u2003", "\u2004", 
-    "\u2005", "\u2006", "\u2007", "\u2008", "\u205F"
+    "\u2005", "\u2006", "\u2007", "\u2008", "\u2009", 
+    "\u205F"
   ]
 
   def encode(string, corpus: nil)
   end
   
-  def convert_character(character)
+  def encode_character(character)
     character.ord.to_s(9).chars.map { |digit|
       STRING_STEGANOGRAPHY[digit.to_i]
-    }
+    } << STRING_STEGANOGRAPHY.last
+  end
+  
+  def encode_string(string)
+    string.chars.inject([]) do |encoded_string, character|
+      encoded_string << encode_character(character)
+    end.flatten
   end
 end
--- spec/unikoder_spec.rb
+++ spec/unikoder_spec.rb
@@ -11,8 +11,15 @@ describe Unikoder do
     @unikoder.encode("hello")
   end
   
-  it "converts a single character into its equivalent space representation" do
-    @unikoder.convert_character("H").must_equal [" ", " "]
-    @unikoder.convert_character("\u2713").must_equal [" ", " ", " ", " ", " "]
+  it "encodes a single character into its equivalent space representation" do
+    @unikoder.encode_character("H").must_equal [" ", " ", " "]
+    @unikoder.encode_character("\u2713").must_equal [" ", " ", " ", " ", " ", " "]
+  end
+  
+  it "encodes a string into its equivalent space representation" do
+    @unikoder.encode_string("Hello World").must_equal [" ", " ", " ", " ", " ", " ", " ", 
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", 
+      " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", 
+      " "]
   end
 end
